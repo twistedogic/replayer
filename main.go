41140551da08ee636d70169671af8f7862415da0
Yat Yee Li <twistedogic@gmail.com>
2025-05-15 00:15:53 +0800

    chore: support multiple series with same metrics name

--- main.go
+++ main.go
@@ -14,57 +14,81 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-type Series struct {
-	Delta  int
+type Override struct {
+	Delta  *int
+	Value  *int
 	Period timeinterval.TimeInterval
 }
 
-func (s Series) update(m *Metrics) bool {
+func (s Override) update(g prometheus.Gauge) bool {
 	if s.Period.ContainsTime(time.Now()) {
-		m.update(s.Delta)
-		return true
+		switch {
+		case s.Value != nil:
+			g.Set(float64(*s.Value))
+			return true
+		case s.Delta != nil:
+			g.Add(float64(*s.Delta))
+			return true
+		}
 	}
 	return false
 }
 
-type Metrics struct {
-	Name      string
-	Interval  model.Duration
-	Delta     int
-	Labels    map[string]string
-	Initial   int
-	Overrides []Series
-	metric    *prometheus.GaugeVec
+type Series struct {
+	Initial, Delta int
+	Interval       model.Duration
+	Labels         map[string]string
+	Overrides      []Override
 }
 
-func (m *Metrics) Register(reg prometheus.Registerer) error {
-	labels := make([]string, 0, len(m.Labels))
-	for l := range m.Labels {
-		labels = append(labels, l)
-	}
-	v := prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: m.Name,
-	}, labels)
-	v.With(m.Labels).Set(float64(m.Initial))
-	if err := reg.Register(v); err != nil {
-		return err
-	}
-	m.metric = v
-	return nil
-}
-
-func (m *Metrics) update(delta int) {
-	log.Printf("update metrics %q with %d", m.Name, delta)
-	m.metric.With(m.Labels).Add(float64(delta))
-}
-
-func (m *Metrics) Update() {
-	for _, s := range m.Overrides {
-		if s.update(m) {
+func (s Series) updateOnce(v prometheus.Gauge) {
+	log.Printf("update metrics %q", v.Desc().String())
+	for _, o := range s.Overrides {
+		if o.update(v) {
 			return
 		}
 	}
-	m.update(m.Delta)
+	v.Add(float64(s.Delta))
+}
+
+func (s Series) startUpdate(vec *prometheus.GaugeVec) {
+	v := vec.With(s.Labels)
+	v.Set(float64(s.Initial))
+	for range time.Tick(time.Duration(s.Interval)) {
+		s.updateOnce(v)
+	}
+}
+
+type Metrics struct {
+	Name   string
+	Series []Series
+}
+
+func (m *Metrics) labelNames() []string {
+	labelMap := make(map[string]struct{})
+	for _, s := range m.Series {
+		for l := range s.Labels {
+			labelMap[l] = struct{}{}
+		}
+	}
+	labels := make([]string, 0, len(labelMap))
+	for name := range labelMap {
+		labels = append(labels, name)
+	}
+	return labels
+}
+
+func (m *Metrics) Register(reg prometheus.Registerer) error {
+	v := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: m.Name,
+	}, m.labelNames())
+	if err := reg.Register(v); err != nil {
+		return err
+	}
+	for _, s := range m.Series {
+		go s.startUpdate(v)
+	}
+	return nil
 }
 
 type Replayer struct {
@@ -87,11 +111,6 @@ func (r Replayer) Start() error {
 		if err := m.Register(prometheus.DefaultRegisterer); err != nil {
 			return err
 		}
-		go func() {
-			for range time.Tick(time.Duration(m.Interval)) {
-				m.Update()
-			}
-		}()
 	}
 	http.Handle("/metrics", promhttp.Handler())
 	return http.ListenAndServe(fmt.Sprintf(":%d", r.Port), nil)
